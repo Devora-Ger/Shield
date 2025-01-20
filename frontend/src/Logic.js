c15105470aada6536507880dc1106b296d29dfff
Devora <Devora0556@gmail.com>
2025-01-20 11:37:11 +0200

    frontend

---
+++ frontend/src/Logic.js
@@ -0,0 +1,15 @@
+export function containsWordsIgnoreCase(text) {
+    const wordsToCheck = ["Suicide", "Kill myself",
+        "End my life","I want to die","Can't go on","Life is pointless","No way out"];
+    let suiciedParam = 0;
+    let numOfSuicideHard = 0;
+    wordsToCheck.map((word, index) => {
+        if(new RegExp(`\\b${word}\\b`, "i").test(text)) {
+            numOfSuicideHard = numOfSuicideHard + 1;
+        }
+    });
+    if (numOfSuicideHard >= 2) {
+        suiciedParam=90;
+    }
+    return suiciedParam;
+}
