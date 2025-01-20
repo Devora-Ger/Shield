b0bf4b1168095be148ea522e68db68eaf92f2ac0
Devora <Devora0556@gmail.com>
2025-01-20 12:16:17 +0200

    split the text

--- frontend/src/Logic.js
+++ frontend/src/Logic.js
@@ -1,15 +1,22 @@
 export function containsWordsIgnoreCase(text) {
-    const wordsToCheck = ["Suicide", "Kill myself",
-        "End my life","I want to die","Can't go on","Life is pointless","No way out"];
-    let suiciedParam = 0;
-    let numOfSuicideHard = 0;
-    wordsToCheck.map((word, index) => {
-        if(new RegExp(`\\b${word}\\b`, "i").test(text)) {
-            numOfSuicideHard = numOfSuicideHard + 1;
+    const suicideWords = [
+        "suicide", "depressed", "hopeless", "alone", "end", 
+        "worthless", "kill", "die", "death", "pain", 
+        "self-harm", "goodbye", "despair", "hurt", "overdose"
+    ];
+    
+    const normalizedText = text.toLowerCase();
+    const wordsInText = normalizedText.split(/\s+/); // Split by whitespace
+    
+    let matchCount = 0;
+    
+    suicideWords.forEach((word) => {
+        if (wordsInText.includes(word)) {
+          matchCount++;
         }
     });
-    if (numOfSuicideHard >= 2) {
-        suiciedParam=90;
-    }
-    return suiciedParam;
+    
+    const matchPercentage = (matchCount / suicideWords.length) * 100;
+    
+    return matchPercentage;
 }
